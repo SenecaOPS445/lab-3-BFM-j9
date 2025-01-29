274b6ee55c1b9b422b920ecbd8fb002c9c599793
Jude Owusu <jowusu9@myseneca.ca>
2025-01-29 16:41:19 -0500

    Lab3a

---
+++ lab3a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#AuthorID: jowusu9
+
+
+#return_text_value() function
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name 
+    return greeting
+
+text = return_text_value()
+
+
+#return_number_value() function
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+
+number = return_number_value()
+
+
+##Main Program
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
